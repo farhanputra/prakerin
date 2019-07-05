8fa6db6a73d2f2ef1bd07186c30a94895def032d
farhan <adenfrhn99@gmail.com>
2019-07-05 14:51:08 +0700

    farhan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Farhan
                 </div>
 
                 <div class="links">
